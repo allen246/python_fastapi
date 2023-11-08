0ee3adea6911b9d5109551871e6a7114aa82a136
allen246 <59170514+allen246@users.noreply.github.com>
2023-11-08 23:42:23 +0530

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -8,20 +8,20 @@ router = APIRouter()
 
 
 @router.get('/me', response_model=schemas.UserResponse)
-def get_me(request: Request, db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_user)):
+async def get_me(request: Request, db: Session = Depends(get_db), user_id: str = Depends(oauth2.require_user)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     setattr(user, 'profile_url', f"{request.base_url}api/image/download/{user.profile_id}")
     del user.profile_id
     return user
 
 @router.get('/all', response_model=List[schemas.AllUserResponse])
-def get_all(request: Request, db: Session = Depends(get_db), verified: str = Depends(oauth2.require_user)):
+async def get_all(request: Request, db: Session = Depends(get_db), verified: str = Depends(oauth2.require_user)):
     response = {}
     users = db.query(models.User).all()
     return users
 
 @router.get('/{user_id}', response_model=schemas.UserResponse)
-def get_user(user_id: str, request: Request, db: Session = Depends(get_db), verified: str = Depends(oauth2.require_user)):
+async def get_user(user_id: str, request: Request, db: Session = Depends(get_db), verified: str = Depends(oauth2.require_user)):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     
     setattr(user, 'profile_url', f"{request.base_url}api/image/download/{user.profile_id}")
